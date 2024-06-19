2e1e14603aa3e657fe9fcc942c51c620cb4bc966
Definev <daiduong.workmail@gmail.com>
2024-06-19 22:23:28 +0700

    feat: Initialize configuration in di_initializer.dart
    Sync the folder structure with lego_architecture_feature and update the initializer in di_initializer.dart to include the configuration module. This allows for better organization and separation of concerns.

--- __brick__/app/{{project_name}}_app/lib/di/di_initializer.dart
+++ __brick__/app/{{project_name}}_app/lib/di/di_initializer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:{{project_name}}_navigator/{{project_name}}_configuration.dart' as configuration;
 import 'package:{{project_name}}_navigator/{{project_name}}_navigator.dart' as nav;
 
 ProviderContainer container = ProviderContainer();
 
 void initialize() {
+  configuration.initialize(container);
   nav.initialize(container);
 }
